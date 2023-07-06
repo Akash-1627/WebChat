87dabbbafb69c958a03d294ab2c816fda3f50873
Akash Chakraborty <akash1627.be21@chitkara.edu.in>
2023-07-06 13:02:18 +0530

    Revert "app.js updated"
    This reverts commit a00d40fc9bfb2e01e2c67a2e18334ab7ae1dd679.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Routes>
         {/* <Route path='/'></Route>
         <Route index element={<ProtectedRoute><Home/></ProtectedRoute>}></Route> */}
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
           <Route
             path="/WebChat"
             element={
